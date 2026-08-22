8ae5c4b2b4d5b62328444e0212afbe71af2e2f93
Lucas <lucassreynoso4@gmail.com>
2026-08-22 00:04:55 -0300

    Limpieza del contenido predeterminado y creación de la estructura de componentes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Brandless</h1>
+      <p>e-commerce ropa con estilo.</p>
+    </div>
+  )
+}
+
+export default App
